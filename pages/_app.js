ce8aae7d06d93ef6e4f842057f3b78df6f01f0ad
success353 <gmacro372@gmail.com>
2022-11-03 01:46:17 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
+}
 export default MyApp
